1e7837d246fab3cc5908f42fa7f710bc1a2b186a
BatFace <echambers0@gmail.com>
2018-04-07 19:07:50 +0100

    Enable CORS for Hangman express server

--- HangmanServer/server/app.js
+++ HangmanServer/server/app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+let app = express();
 
 const { auth, users, games } = require("./routes");
 
 mongoose.connect(process.env.DB_CONNECTION_STRING);
 
+app.use(cors({
+  origin: process.env.HANGMAN_CLIENT_URL
+}));
 app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/games", games);
