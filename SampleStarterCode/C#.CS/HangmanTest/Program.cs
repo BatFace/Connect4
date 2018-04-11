9ea995935e1940f7cf2fd78f0f877ac375db561b
BatFace <echambers0@gmail.com>
2018-04-11 15:41:36 +0100

    Add C# starter code (Credit: @DavidBetteridge)

---
+++ SampleStarterCode/C#.CS/HangmanTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using HangmanTest.Services;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HangmanTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var hangmanServer = HangmanServer();
+
+                // NOTE - Call RegisterUserOnServerAction just once,  and then remember the value returned for x-access-token for next time
+                var registerUserAction = new RegisterUserOnServerAction(hangmanServer);
+                var x_access_token = await registerUserAction.Execute(username: "CHANGE_ME", password: "CHANGE_ME");
+
+                Login(hangmanServer, x_access_token);
+
+                var newGameAction = new StartNewGameOnServerAction(hangmanServer);
+                var newGame = await newGameAction.Execute();
+
+                var allGamesQuery = new AllGamesOnServerQuery(hangmanServer);
+                var allGames = await allGamesQuery.Evalulate();
+
+                var currentGameQuery = new CurrentGameOnServerQuery(hangmanServer);
+                var game = await currentGameQuery.Evalulate();
+
+                var specificGameQuery = new SpecificGameOnServerQuery(hangmanServer);
+                var specificGame = await specificGameQuery.Evalulate(allGames.First().id);
+
+                var makeMoveAction = new PlayMoveOnServerAction(hangmanServer);
+                game = await makeMoveAction.Execute('I');
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
+        }
+
+        private static void Login(HttpClient hangmanServer, string x_access_token)
+        {
+            hangmanServer.DefaultRequestHeaders.Add("x-access-token", x_access_token);
+        }
+
+        private static HttpClient HangmanServer()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://dojo-hangman-server.herokuapp.com")
+            };
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+
+
+
+}
--- SampleStarterCode/C#.CS/HangmanTest/Properties/AssemblyInfo.cs
+++ SampleStarterCode/C#.CS/HangmanTest/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Connect4.ExampleBot")]
+[assembly: AssemblyTitle("HangmanTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Connect4.ExampleBot")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("HangmanTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d23ac326-b0c5-4d5d-96fa-21d6db4f8653")]
+[assembly: Guid("c19553c7-fc61-4cf4-8c34-5a16de45e5e0")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
